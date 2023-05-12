a3ef22e0dae3dd5025d0be8d3caaac1e298e0d4d
OsamaShabbir508 <56590799+OsamaShabbir508@users.noreply.github.com>
2023-05-12 17:17:25 +0500

    added some changes

--- src/component/OfferCard/OfferCard.jsx
+++ src/component/OfferCard/OfferCard.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
-import HowItsWork from '../../assets/works.png'
 import Vector1 from '../../assets/Vector1.png'
 import Vector2 from '../../assets/Vector2.png'
 import Immigration from '../../assets/Immigration.png'
-// sds
-export default function OfferCard() {
+
+export default function OfferCard({image,age,name,hours,person}) {
     return (
-        <div className='bg-[#F0F4FF] w-[290px] h-96 flex flex-row justify-center p-4  m-3 rounded-sm'>
-            <div className='bg-white'>
-                <img src={HowItsWork} className='w-64 h-44'></img>
+        <div className='bg-[#F0F4FF] w-[260px]   flex flex-row justify-center p-4  m-3 rounded-sm'>
+            <div className='bg-white pb-4'>
+                <img src={image} className='w-64 h-44'></img>
                 <div className='ml-4'>
-                <h1 className='font-bold text-md mt-4'>Smith & Jones</h1>
+                <h1 className='font-bold text-md mt-4'>{name}</h1>
                 <div className='flex  flex-row items-center flex-wrap mt-1'>
                     <p className=' flex  flex-col items-center justify-center pb-1  '><img src={Vector1}></img></p>
-                    <p className='ml-2'>45 Years </p>
+                    <p className='ml-2'>{age} </p>
                 </div>
                 <div className='flex  flex-row items-center flex-wrap  mt-1'>
                     <p className=' flex  flex-col items-center justify-center pb-1  '><img src={Vector2}></img></p>
-                    <p className='ml-2'>290 pro bono hours </p>
+                    <p className='ml-2'>{hours}</p>
                 </div>
                 <div className='flex  flex-row items-center flex-wrap  mt-1'>
                     <p className=' flex  flex-col items-center justify-center pb-1  '><img src={Immigration}></img></p>
-                    <p className='ml-2'>Immigration</p>
+                    <p className='ml-2'>{person}</p>
                 </div>
                 </div>
             </div>
