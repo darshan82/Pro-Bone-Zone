357e71e78a94b8430f0d9fcd9956238e90fbbe8f
OsamaShabbir508 <56590799+OsamaShabbir508@users.noreply.github.com>
2023-05-20 17:50:28 +0500

    added email js issue resolved

--- src/Homepage/index.jsx
+++ src/Homepage/index.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import Navbar from "../component/Navbar/navbar";
 import SolutionCard from "../component/SolutionCard/SolutionCard";
 import Button from "../component/button/button";
@@ -45,20 +45,29 @@ export default function index() {
   };
   const sendEmail = (e) => {
     e.preventDefault();
-  
+    //console.log(e.target);
+
     // const formData = new FormData(form.current);
     // const formValues = Object.fromEntries(formData);
 
     // // Do something with the form values
     // console.log(formValues);
 
-
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_m7tcc4g",
+        "template_ty2kqgc",
+        form.current,
+        "2Z-yhu6rbMDiGx-KA"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
   return (
     <>
@@ -66,7 +75,7 @@ export default function index() {
         <Navbar />
         {/* Hero Section */}
         <div className="lg:ml-44">
-          <h1 className="text-center text-[#2E5FB7] sm:text-center md:text-center lg:text-left font-inter font-semibold  text-3xl lg:text-4xl leading-10  lg:w-[450px] w-full  my-1  pt-10">
+          <h1 className="text-center text-[#2E5FB7] sm:text-center md:text-center lg:text-left font-inter font-semibold   text-[22px] md:text-3xl lg:text-4xl leading-10  lg:w-[450px] w-full  my-1  pt-10">
             Connecting. Empowering. Elevating Lives.
           </h1>
 
@@ -89,14 +98,14 @@ export default function index() {
               </div>
             </div>
 
-           
-             <img
+            <img
               src={HeroImage}
               className="hidden lg:block md:block md: w-[500px] lg:w-[35%] mt-5 lg:ml-[60px] lg:mt-[-35px]"
-             // className=" w-[500px] lg:w-[35%] mt-5 lg:ml-[60px] lg:mt-[-35px]"
+              // className=" w-[500px] lg:w-[35%] mt-5 lg:ml-[60px] lg:mt-[-35px]"
             ></img>
-            <img src={HeroImageLessBorder}
-            className="sm:block sm:w-[500px] md:hidden lg:hidden"
+            <img
+              src={HeroImageLessBorder}
+              className="sm:block sm:w-[500px] md:hidden lg:hidden"
             />
           </div>
         </div>
@@ -108,13 +117,13 @@ export default function index() {
           // className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 mx-auto"
           className=" flex  flex-col   items-center justify-center   sm:flex-col md:flex-row md:justify-start md:items-start  lg:flex-row lg:justify-start md:ml-20 lg:ml-44 "
         >
-          <div className="sm:w-96 md:w-[338px] lg:w-[450px]">
+          <div className="sm:w-[620px] md:w-[338px] lg:w-[450px] bg-black">
             <YouTube
               //  className=" max-h-96  max-w-sm"
               videoId={videId}
               controls={true}
               opts={{
-                width: "110%",
+                width: "100%",
                 height: "400px",
               }}
               //  playsinline={true}
@@ -168,7 +177,7 @@ export default function index() {
               icon={TheProblem}
               heading={"ANXIETY"}
               para="Why do we hesitate to talk with an attorney? We are embarrassed.
-            We fear judgement and insensitivity."
+            We fear judgement and insensitivity. We avoid change and commitment. We worry about the cost"
             />
             <SolutionCard
               icon={Procrastination}
@@ -270,7 +279,7 @@ export default function index() {
               />
             </div>{" "}
           </div>
-          <div className=" sm:w-full md:w-[40%] mt-4 lg:w-[40%] flex flex-col justify-center items-center ml-8  ">
+          <div className="ml-0  w-full md:w-[40%] md:ml-3 mt-4 lg:w-[40%] flex flex-col justify-center items-center lg:ml-8  ">
             <img
               src={YourSolutionImage}
               className="w-[300px] h-[300px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px]  "
@@ -413,74 +422,85 @@ export default function index() {
           </div>
           <div className="w-[100%] md:w-[50%] lg:w-[50%]  ">
             <form ref={form} onSubmit={sendEmail}>
-            <div className="flex flex-col justify-center md:flex-row lg:flex-row  flex-wrap   ">
-              <input
-                type="text"
-                className="py-2 px-3 bg-gray-100 text-sm text-[#414141] rounded-sm m-2 "
-                placeholder="First Name"
-                name="firstName"
-              ></input>
-              <input
-                type="text"
-                className="py-2 px-3 bg-gray-100  text-sm text-[#414141] rounded-sm m-2"
-                placeholder="Last Name"
-                name="lastName"
-              ></input>
-             
-            </div>
-            <div className="flex flex-col justify-center md:flex-row lg:flex-row  flex-wrap  ">
-              <input
-                type="text"
-                className="py-2 px-3 bg-gray-100  text-sm text-[#414141] rounded-sm m-2"
-                placeholder="Email"
-                name="email"
-              ></input>
-              <input
-                type="text"
-                className="py-2 px-3 bg-gray-100  text-sm text-[#414141] rounded-sm m-2"
-                placeholder="Phone"
-                name="phone"
-              ></input>
-            </div>
-            <div className="flex flex-col justify-center md:flex-row lg:flex-row  flex-wrap  ">
-              <select name="type" className="py-2 px-10  bg-gray-100 text-sm text-[#414141] rounded-sm m-2">
-                <option value="family-law">Family Law</option>
-                <option value="business-law">Business Law</option>
-              </select>
-              {/* <input
-                type="text"
-                className="py-2 px-3 bg-gray-100  text-sm text-[#414141] rounded-sm m-2"
-                placeholder="Category"
-              ></input> */}
-              <select name="time" className="py-2 px-7  bg-gray-100 text-sm text-[#414141] rounded-sm m-2">
-                <option value="">Select a time slot</option>
-                <option value="10:00">10:00 a.m.</option>
-                <option value="10:30">10:30 a.m.</option>
-                <option value="11:00">11:00 a.m.</option>
-                <option value="11:30">11:30 a.m.</option>
-                <option value="12:00">12:00 p.m.</option>
-                <option value="12:30">12:30 p.m.</option>
-                <option value="13:00">1:00 p.m.</option>
-                <option value="13:30">1:30 p.m.</option>
-              </select>
-            </div>
+              <div className="flex flex-col justify-center  md:flex-row lg:flex-row  flex-wrap   ">
+                <input
+                  type="text"
+                  className="py-2 px-3 bg-gray-100 text-sm text-[#414141] rounded-sm m-2 "
+                  placeholder="First Name"
+                  name="firstName"
+                ></input>
+                <input
+                  type="text"
+                  className="py-2 px-3 bg-gray-100  text-sm text-[#414141] rounded-sm m-2"
+                  placeholder="Last Name"
+                  name="lastName"
+                ></input>
+              </div>
+              <div className="flex flex-col justify-center  md:flex-row lg:flex-row  flex-wrap  ">
+                <input
+                  type="text"
+                  className="py-2 px-3 bg-gray-100  text-sm text-[#414141] rounded-sm m-2"
+                  placeholder="Email"
+                  name="email"
+                ></input>
+                <input
+                  type="text"
+                  className="py-2 px-3 bg-gray-100  text-sm text-[#414141] rounded-sm m-2"
+                  placeholder="Phone"
+                  name="phone"
+                ></input>
+              </div>
 
-            <div className="flex flex-col justify-center md:flex-row lg:flex-row  flex-wrap  ">
-              <textarea
-                name="description"
-                id=""
-                placeholder="How did you hear about Pro Bono Zone?"
-                className="bg-gray-100 p-4   md:w-96 lg:w-96 h-20 m-2"
-              ></textarea>
-            </div>
-            <div className=" flex justify-center mt-3 md:justify-center lg:justify-center ">
-              <button type="submit"   className="bg-[#EC672C] w-full  py-2 rounded-lg text-white text-sm mx-1 lg:mx-4 lg:w-96">
-                Count Me In
-              </button>
-            </div>
+              <div className="flex flex-col justify-center flex-wrap   md:flex-row lg:flex-row">
+                <select
+                  name="type"
+                  className="h-10  md:w-[184px] lg:px-3 lg:py-2 bg-gray-100 text-sm text-[#414141] rounded-sm m-2"
+                >
+                  <option value="" disabled selected>
+                    Select a Law
+                  </option>
+
+                  <option value="family-law">Family Law</option>
+                <option value="business-law">Business Law</option>
+                </select>
+
+                <select
+                  name="time"
+                  className="h-10  md:w-[184px] lg:px-3 lg:py-2 bg-gray-100 text-sm text-[#414141] rounded-sm m-2"
+                >
+                  <option value="" disabled selected>
+                    Select a Time
+                  </option>
+
+                  <option value="10:00">10:00 a.m.</option>
+                  <option value="10:30">10:30 a.m.</option>
+                  <option value="11:00">11:00 a.m.</option>
+                  <option value="11:30">11:30 a.m.</option>
+                  <option value="12:00">12:00 p.m.</option>
+                  <option value="12:30">12:30 p.m.</option>
+                  <option value="13:00">1:00 p.m.</option>
+                  <option value="13:30">1:30 p.m.</option>
+                </select>
+              </div>
+
+              <div className="flex flex-col justify-center md:flex-row lg:flex-row  flex-wrap  ">
+                <textarea
+                  name="description"
+                  id=""
+                  placeholder="How did you hear about Pro Bono Zone?"
+                  className="bg-gray-100 p-4   md:w-96 lg:w-96 h-20 m-2"
+                ></textarea>
+              </div>
+              <div className=" flex justify-center mt-3 md:justify-center lg:justify-center ">
+                <button
+                  type="submit"
+                  className="bg-[#EC672C] w-full  py-2 rounded-lg text-white text-sm mx-1 lg:mx-4 lg:w-96"
+                >
+                  Count Me In
+                </button>
+              </div>
             </form>
           </div>
-          
         </div>
       </div>
 
@@ -500,10 +520,10 @@ export default function index() {
           </div>
         </div> */}
         <div
-          className="flex flex-row flex-wrap justify-center items-center text-center md:text-left lg:text-left md:flex-row  md:justify-between md:items-start lg:flex-row lg:items-start lg:justify-between lg:w-[80%]  lg:ml-44 py-5 lg:pb-28"
-          style={{ paddingTop: 100 }}
+          className="flex flex-row flex-wrap justify-center items-center text-center pt-16 md:text-left lg:text-left md:flex-row  md:justify-between md:items-start lg:flex-row lg:items-start lg:justify-between lg:w-[80%]  lg:ml-44 py-5 lg:pb-28"
+          //style={{ paddingTop: 40 }}
         >
-          <div className="text-white flex flex-col items-center md:items-start  lg:items-start    py-4 h-56 w-[250px] sm:[280px]  md:w-auto lg:w-auto">
+          <div className="text-white flex flex-col items-center md:items-start  lg:items-start    py-4 h-24 w-[250px] sm:[280px]  md:w-auto lg:w-auto">
             <img src={Logo} className="w-52 md:w-44 lg:w-52"></img>
             {/* <p className="mt-3 text-[#727581]">
               &copy; 2023 Pro Bono Zone&trade;
@@ -517,7 +537,7 @@ export default function index() {
               <img src={YoutubeWhite}></img>
             </div> */}
           </div>
-          <div className="py-4 h-56 w-[250px] sm:[280px]  md:w-auto lg:w-auto text-[#727581]">
+          <div className="py-4 h-56 w-[250px] sm:[280px] mb-12  md:w-auto lg:w-auto text-[#727581]">
             <h1 className="font-semibold text-lg">Website</h1>
             <p className="mt-3">Home</p>
             <p className="mt-3">About</p>
@@ -526,7 +546,7 @@ export default function index() {
             <p className="mt-3">Contact</p>
           </div>
 
-          <div className=" py-4 h-56  w-[250px] sm:[280px]  md:w-auto lg:w-auto text-[#727581]">
+          <div className=" py-4 h-56   w-[250px] sm:[280px]  md:w-auto lg:w-auto text-[#727581]">
             <h1 className="font-semibold text-lg">Legal</h1>
             <p className="mt-3">Terms of Service</p>
             {/* <p className="mt-3">Legal</p> */}
