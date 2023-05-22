1d04ba34de1a255e65bd1a8b20229d800a377d13
Darshan Lal <darshanmandhwani12@gmail.com>
2023-05-23 00:20:13 +0500

    icons

--- src/component/OfferCard/OfferCard.jsx
+++ src/component/OfferCard/OfferCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Vector1 from '../../assets/Vector1.png'
-import Vector2 from '../../assets/Vector2.png'
+import Vector2 from '../../assets/Time.png'
 import Immigration from '../../assets/Immigration.png'
 
 export default function OfferCard({image,age,name,hours,person}) {
@@ -11,15 +11,15 @@ export default function OfferCard({image,age,name,hours,person}) {
                 <div className='ml-4'>
                 <h1 className='font-bold text-md mt-4'>{name}</h1>
                 <div className='flex  flex-row items-center flex-wrap mt-1'>
-                    <p className=' flex  flex-col items-center justify-center pb-1  '><img src={Vector1}></img></p>
+                    <p className=' flex  flex-col items-center justify-center pb-1 '><img width={"50"} height={50} src={Vector1}></img></p>
                     <p className='ml-2'>{age} </p>
                 </div>
                 <div className='flex  flex-row items-center flex-wrap  mt-1'>
-                    <p className=' flex  flex-col items-center justify-center pb-1  '><img src={Vector2}></img></p>
-                    <p className='ml-2'>{hours}</p>
+                    <p className=' flex  flex-col items-center justify-center pb-1 ml-2 '><img width={"35"} height={50}  src={Vector2}></img></p>
+                    <p className='ml-4'>{hours}</p>
                 </div>
                 <div className='flex  flex-row items-center flex-wrap  mt-1'>
-                    <p className=' flex  flex-col items-center justify-center pb-1  '><img src={Immigration}></img></p>
+                    <p className=' flex  flex-col items-center justify-center pb-1  '><img width={"50"} height={50}  src={Immigration}></img></p>
                     <p className='ml-2'>{person}</p>
                 </div>
                 </div>
