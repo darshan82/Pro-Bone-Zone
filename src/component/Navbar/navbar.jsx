2aed87de18c8a8eda089945547ed44110924df22
OsamaShabbir508 <56590799+OsamaShabbir508@users.noreply.github.com>
2023-05-23 23:36:10 +0500

    new changes

--- src/component/Navbar/navbar.jsx
+++ src/component/Navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 // import Logo from '../../assets/Group6.png'
 import Logo from "../../assets/Logo.png";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -30,8 +30,45 @@ export default function navbar() {
             </svg>
           </button>
         </div>
-      
-</nav>
+        {isNavOpen && (
+          <div className="px-6 pt-3 ">
+            <Link to={"/"}>
+              <p className="cursor-pointer text-[#414141] text-center my-2 block">
+                Home
+              </p>
+            </Link>
+            <Link to={'/about'}>
+              <p className="cursor-pointer text-[#414141] text-center my-2 block">
+                About
+              </p>
+            </Link>
+            <Link to={'/faq'}>
+              <p className="cursor-pointer text-[#414141] text-center my-2 block">
+                FAQ
+              </p>
+            </Link>
+            <Link to={'/solution'}>
+            <p className="cursor-pointer text-[#414141] text-center my-2 block">
+              Sponsors
+            </p>
+            </Link>
+            <p className="cursor-pointer text-[#414141] text-center my-2 block">
+              Contact
+            </p>
+            <div className="flex justify-center ">
+              <button className="bg-[#EC672C] px-8 py-2 w-44 rounded-sm text-white my-2 block">
+                Register
+              </button>
+            </div>
+            <div className="flex justify-center">
+              <button className="bg-[#f8f9fd] border  border-[#414141] text-[#EC672C] px-8 py-2 rounded-md cursor-pointer">
+                Login
+              </button>
+            </div>
+          </div>
+        )}
+      </nav>
+
       {/* Desktop Navigation */}
       <nav className="hidden md:flex md:flex-row md:justify-between md:items-center md:px-16 md:py-3 lg:flex lg:flex-row lg:justify-between lg:items-center  lg:pl-44 bg-[#EAEFF8] lg:pt-9  ">
         <div className="">
@@ -59,8 +96,8 @@ export default function navbar() {
           </button>
         </div>
       </nav>
-    
-    {/* //     <>
+    </>
+    //     <>
     //     <div className='sm:flex sm:flex-row sm:justify-between sm:items-center sm:px-6 sm:py-3 md:hidden'>
     //   <div className=''>
     //     <img src={Logo} className='w-16'></img>
@@ -68,15 +105,15 @@ export default function navbar() {
     //   <button className='flex items-center text-white focus:outline-none'>
     //     <svg className='h-6 w-6 fill-current' viewBox='0 0 24 24'>
     //       <path
-            fill-rule='evenodd'
-            clip-rule='evenodd'
-            d='M3 18h18v-2H3v2zM3 13h18v-2H3v2zM3 6v2h18V6H3z'
-          />
-        </svg>
-      </button>
-    </div> */}
+    //         fill-rule='evenodd'
+    //         clip-rule='evenodd'
+    //         d='M3 18h18v-2H3v2zM3 13h18v-2H3v2zM3 6v2h18V6H3z'
+    //       />
+    //     </svg>
+    //   </button>
+    // </div>
 
-    {/* //     <div className='hidden sm:hidden md:flex md:flex-row md:justify-between md:items-center md:px-16 md:py-3 lg:flex lg:flex-row lg:justify-between lg:items-center lg:px-16 lg:py-3 '>
+    //     <div className='hidden sm:hidden md:flex md:flex-row md:justify-between md:items-center md:px-16 md:py-3 lg:flex lg:flex-row lg:justify-between lg:items-center lg:px-16 lg:py-3 '>
     //     <div className=''>
     //       <img src={Logo} className='w-20'></img>
     //     </div>
@@ -90,7 +127,6 @@ export default function navbar() {
     //       <button className='bg-[#4270FF] px-8 py-2 rounded-sm'>Register</button>
     //     </div>
     //     </div>
-    //     </> */}
-    </>
+    //     </>
   );
 }
