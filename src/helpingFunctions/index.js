3e7815671e29d33b82cdf43506edf9c916d01edb
Pirthivi <pirthivi66@gmail.com>
2023-06-17 04:27:02 -0700

    27

--- src/helpingFunctions/index.js
+++ src/helpingFunctions/index.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const setBaseUrl = ()=>{
-    // axios.defaults.baseURL = "http://167.172.154.189:3000"
-    axios.defaults.baseURL = "http://localhost:3000"
+    axios.defaults.baseURL = "http://167.172.154.189:3000"
+    // axios.defaults.baseURL = "http://localhost:3000"
 
 }
 
