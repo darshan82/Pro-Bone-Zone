8b5cb873b392a330d462993f43fe39c0c5c1245e
Darshan Lal <darshanmandhwani12@gmail.com>
2023-06-17 11:33:43 +0500

    code

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ const storedUser = JSON.parse(localStorage.getItem("user"))
 export const userType = storedUser?.permit
 export const userTypes = {
     admin: "admin",
-    licensee: "licensee",
+    licensee: "licensees",
     staff: "staff"
 }
 
