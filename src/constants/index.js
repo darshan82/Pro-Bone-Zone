c89bbfb592999b8d975d2827faf15a1756dd2510
Darshan Lal <darshanmandhwani12@gmail.com>
2023-07-11 23:21:26 +0500

    time

--- src/constants/index.js
+++ src/constants/index.js
@@ -76,7 +76,7 @@ export const ratings = [1, 2, 3, 4, 5]
 
 
 // events
-export const timeList = ["9:00 AM","9:30 AM","10:00 AM","10:30 AM","11:00 AM","11:30 AM","12 noon","12:30 PM","1:00 PM","1:30 PM","2:00 PM","2:30 PM","3:00 PM","3:30 PM","4:00 PM","4:30 PM","5:00 PM","5:30 PM","6:00 PM","6:30 PM","7:00 PM","7:30 PM","8:00 PM","8:30 PM","9:00 PM","9:30 PM","10:00 PM"]
+export const timeList = ["9:00 AM","9:30 AM","10:00 AM","10:30 AM","11:00 AM","11:30 AM","12:00 PM","12:30 PM","1:00 PM","1:30 PM","2:00 PM","2:30 PM","3:00 PM","3:30 PM","4:00 PM","4:30 PM","5:00 PM","5:30 PM","6:00 PM","6:30 PM","7:00 PM","7:30 PM","8:00 PM","8:30 PM","9:00 PM","9:30 PM","10:00 PM"]
 export const eventTypes = [{label:"Legal" , value:"Legal"},{label:"Business" , value:"Business"},{label:"Financial" , value:"Financial"}]
 //  promotions
 export const PromotionTypes = [{label:"Legal" , value:"Legal"},{label:"Business" , value:"Business"},{label:"Financial" , value:"Financial"}]
